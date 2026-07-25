0c1cb676b28f1167e80ea7840869f05df6357db5
carmencnles <mcarmencanales1@gmail.com>
2026-07-25 21:45:41 +0200

    a

---
+++ Basico/00-SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/Ejercicio0.py
@@ -0,0 +1,40 @@
+"""
+  EJERCICIO:
+  - Crea un comentario en el código y coloca la URL del sitio web oficial del
+    lenguaje de programación que has seleccionado.
+  - Representa las diferentes sintaxis que existen de crear comentarios
+    en el lenguaje (en una línea, varias...).
+  - Crea una variable (y una constante si el lenguaje lo soporta).
+  - Crea variables representando todos los tipos de datos primitivos
+    del lenguaje (cadenas de texto, enteros, booleanos...).
+  - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ 
+  ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+  debemos comenzar por el principio.
+"""
+
+# 1- Python: URL = https://www.python.org
+
+# 2- Se puede comentar con una sola linea(#), y con """ y con '''
+
+variable1 = 1
+
+MY_CONSTANT =  'Mi constante ' # por convención
+
+variable_int_enteros = 10
+
+variable_float_flotantes = 3.14
+
+variable_str_cadenas = 'Hola Mundo'
+
+variable_bool_booleanos = True
+
+variable_bool1_booleanos = False
+
+
+print('Hola, Python!')
+
+print(type(variable_int_enteros))
+print(type(variable_float_flotantes))
+print(type(variable_str_cadenas))
+print(type(variable_bool1_booleanos))

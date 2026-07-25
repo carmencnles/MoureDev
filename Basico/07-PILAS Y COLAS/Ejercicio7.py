3ad6e8c3702dc7d8fabb88c0da4f678314e590c5
carmencnles <mcarmencanales1@gmail.com>
2026-07-25 22:59:00 +0200

    a

--- Basico/07-PILAS Y COLAS/Ejercicio7.py
+++ Basico/07-PILAS Y COLAS/Ejercicio7.py
@@ -3,7 +3,45 @@ EJERCICIO:
  * Implementa los mecanismos de introducción y recuperación de elementos propios de las
  * pilas (stacks - LIFO) y las colas (queue - FIFO) utilizando una estructura de array
  * o lista (dependiendo de las posibilidades de tu lenguaje).
- *
+ '''
+
+import queue
+
+q1 = queue.Queue()
+
+
+q1.put(1)
+q1.put(5)
+q1.put(7)
+q1.put(9)
+q1.put(10)
+
+print(q1.full())
+
+#print(type(q1))
+
+item1 = q1.get()
+
+print('The item removed from the queue is ', item1)
+
+item2 = q1.get()
+
+print('The item removed from the queue is ', item2)
+
+
+lista = []
+
+lista.append(50)
+lista.append(30)
+lista.append(20)
+lista.append(10)
+
+ultimo = lista.pop()
+print(f"Saco el {ultimo}")
+
+print("Pila después de sacar un libro de la caja:", lista)
+
+'''
  * DIFICULTAD EXTRA (opcional):
  * - Utilizando la implementación de pila y cadenas de texto, simula el mecanismo adelante/atrás
  *   de un navegador web. Crea un programa en el que puedas navegar a una página o indicarle
--- Basico/08-CLASES/Ejercicio8.py
+++ Basico/08-CLASES/Ejercicio8.py
@@ -5,7 +5,31 @@
  * de tu lenguaje).
  * Una vez implementada, créala, establece sus parámetros, modifícalos e imprímelos
  * utilizando su función.
- *
+ '''
+
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def getNombre(self):
+        return self.nombre
+
+    def getEdad(self):
+        return self.edad
+
+    def saludar(self):
+        print(f"Hola, soy {self.getNombre()}")
+
+usuario1 = Persona("carmen", 22)
+
+usuario1.saludar()
+
+print(f"Edad: {usuario1.getEdad()}")
+
+
+
+'''
  * DIFICULTAD EXTRA (opcional):
  * Implementa dos clases que representen las estructuras de Pila y Cola (estudiadas
  * en el ejercicio número 7 de la ruta de estudio)
@@ -13,3 +37,52 @@
  *   retornar el número de elementos e imprimir todo su contenido.
 
 '''
+
+
+class LIFO:
+    def __init__(self):
+        self.pila = []
+
+    def añadirElemento(self, elemento):
+        self.pila.append(elemento)
+
+    def eliminarElemento(self):
+        self.pila.pop()
+
+    def imprimirPila(self):
+        print(self.pila)
+
+lifo1 = LIFO()
+lifo1.imprimirPila()
+lifo1.añadirElemento(1)
+lifo1.imprimirPila()
+lifo1.añadirElemento(2)
+lifo1.imprimirPila()
+lifo1.eliminarElemento()
+lifo1.imprimirPila()
+
+
+import queue
+
+class FIFO:
+    def __init__(self):
+        self.cola = queue.Queue()
+
+    def añadirElementoCola(self, elemento):
+        self.cola.put(elemento)
+
+    def eliminarElementoCola(self):
+        self.cola.get()
+
+    def imprimirCola(self):
+        print(self.cola.queue)
+
+
+cola1 = FIFO()
+cola1.imprimirCola()
+cola1.añadirElementoCola(1)
+cola1.imprimirCola()
+cola1.añadirElementoCola(2)
+cola1.imprimirCola()
+cola1.eliminarElementoCola()
+cola1.imprimirCola()
